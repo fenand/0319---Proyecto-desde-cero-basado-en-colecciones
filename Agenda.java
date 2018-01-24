71dbdf3d144b0ff4fc3037421e8c967a4015a809
Fenand <garciaf4c@gmail.com>
2018-01-24 10:46:00 +0100

    Creada la clase agenda y paginasAmarillas

---
+++ Agenda.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class Agenda here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Agenda
+{
+    
+    //Nombre de la persona en la agenda 
+    private String Contacto;
+    // numero de telefono de los contactos de la agenda
+    private int numeroDeTelefono;
+    //direccion de la persona
+    private String direccion;
+
+    /**
+     * Constructor for objects of class Agenda
+     */
+    public Agenda()
+    {
+     
+    }
+
+}
